3795de439c469a4b0d5a2e636389c489b650fd91
Jardel Rocha <106354939+del-iron@users.noreply.github.com>
2025-04-16 09:17:07 -0300

    testando

--- paramentros.php
+++ paramentros.php
@@ -8,6 +8,7 @@ class paramentros {
     private static $dbHost = 'localhost';
     private static $dbName = 'chatbot_moodle';
     private static $dbUser = 'root';
+    private static $dbPass = 'root';
     private static $dbCharset = 'utf8mb4';
 
     // Método para obter a conexão com o banco de dados
